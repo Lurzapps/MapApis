efdb2603e2fcf7f3ad3ddd16ceae02043b8efe02
Lurzapps <lurzapps@gmail.com>
2020-03-27 14:52:55 +0100

    Create Forecast.java

---
+++ com/lurzapps/mapapis/weathermodel/Forecast.java
@@ -0,0 +1,16 @@
+package com.lurzapps.mapapis.weathermodel;
+
+/**
+ * Created by Lurzapps on 26.03.2020
+ * Copyright Lurzapps (2020)
+ * Package: com.lurzapps.mapapis.weathermodel
+ */
+public class Forecast {
+    public String time;
+    public String waterTemp, airTemp, airTempFeelsLike;
+    public String weatherDesc;
+    public String windSpeed, windDirCompass;
+    public String rainMM, humidity, cloudCover;
+    public String swellHeight, swellPeriod, sigHeight, swellDirCompass;
+    public String visibility;
+}
