55acb2b9d3c29bce3ff99b5c1b0763577e3d0195
Lurzapps <lurzapps@gmail.com>
2020-03-27 14:54:15 +0100

    Create Tide.java

---
+++ com/lurzapps/mapapis/weathermodel/Tide.java
@@ -0,0 +1,12 @@
+package com.lurzapps.mapapis.weathermodel;
+
+/**
+ * Created by Lurzapps on 26.03.2020
+ * Copyright Lurzapps (2020)
+ * Package: com.lurzapps.mapapis.weathermodel
+ */
+public class Tide {
+    public boolean lowTide;
+    public String tideTime;
+    public String tideHeightMt;
+}
